9bc289f55dc91d0a62e4e00ca72cf519a1cc0c9d
Patrick Luhmann <patrick.luhmann@gmail.com>
2017-02-07 13:55:24 -0800

    Add Button Response activity
    Added a new activity for the Button Response sample. Because it is the first sample,
    the onListItemClick method is also created to access it. The new activity has text
    and a button, but it doesn't actually do anything yet.
    
    Signed-off-by: Patrick Luhmann <patrick.luhmann@gmail.com>

---
+++ app/src/main/java/com/neurdyneutrino/modron_monodrone/ButtonResponse.java
@@ -0,0 +1,26 @@
+package com.neurdyneutrino.modron_monodrone;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ButtonResponse extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_button_response);
+
+		TextView msg = new TextView(this);
+		msg.setText("Welcome to the Button Response sample");
+
+		ViewGroup layout = (ViewGroup)findViewById(R.id.activity_button_response);
+		layout.addView(msg);
+
+		Button btn = new Button(this);
+		btn.setText("Press Me!");
+		layout.addView(btn);
+	}
+}
--- app/src/main/java/com/neurdyneutrino/modron_monodrone/SampleStarter.java
+++ app/src/main/java/com/neurdyneutrino/modron_monodrone/SampleStarter.java
@@ -1,16 +1,38 @@
 package com.neurdyneutrino.modron_monodrone;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class SampleStarter extends ListActivity {
-	String samples[] = {"Sample 1", "Sample 2"};
+	String samples[] = {
+		"Button Response",
+		"Sample 2"
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, samples));
 	}
+	@Override
+	protected void onListItemClick(ListView list, View view, int position, long id) {
+		super.onListItemClick(list, view, position, id);
+
+		// TODO: There is probably a more elegant or "Android" way of doing this.
+		switch (position)
+		{
+			case 0:
+				Intent intent = new Intent(this, ButtonResponse.class);
+				startActivity(intent);
+				break;
+			default:
+				break;
+		}
+	}
+
 }
