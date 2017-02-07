094a5d6e03a463033088fa1f91e140563d48ac93
Patrick Luhmann <patrick.luhmann@gmail.com>
2017-02-07 10:41:27 -0800

    Add initial project files
    Signed-off-by: Patrick Luhmann <patrick.luhmann@gmail.com>

---
+++ app/src/main/java/com/neurdyneutrino/modron_monodrone/SampleStarter.java
@@ -0,0 +1,13 @@
+package com.neurdyneutrino.modron_monodrone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SampleStarter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample_starter);
+    }
+}
