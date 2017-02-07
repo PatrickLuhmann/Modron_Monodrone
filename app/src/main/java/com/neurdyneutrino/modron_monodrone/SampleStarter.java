1fee1951a7fe1bc66e79a01a4e7b6cfeeb9014b5
Patrick Luhmann <patrick.luhmann@gmail.com>
2017-02-07 13:07:24 -0800

    Start activity and title bar learnings
    The start activity is now a list of samples, although no sample has been
    implemented yet.
    There was some problem with displaying the title bar. The problem has to
    do with Theme.AppCompat, but I couldn't figure out exactly what. The name
    resolves to something, but it doesn't do anything. After searching online,
    I learned that using a different theme, such as Holo, results in the title
    bar being displayed. There are now several app-defined themes to choose
    from.
    
    Converted spaces to tabs on several files.
    
    Signed-off-by: Patrick Luhmann <patrick.luhmann@gmail.com>

--- app/src/main/java/com/neurdyneutrino/modron_monodrone/SampleStarter.java
+++ app/src/main/java/com/neurdyneutrino/modron_monodrone/SampleStarter.java
@@ -1,13 +1,16 @@
 package com.neurdyneutrino.modron_monodrone;
 
+import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
-public class SampleStarter extends AppCompatActivity {
+public class SampleStarter extends ListActivity {
+	String samples[] = {"Sample 1", "Sample 2"};
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sample_starter);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, samples));
+	}
 }
